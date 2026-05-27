954b10363458dc39c8618e1c007ee78c8b74bc2d
KZY0606 <KZY0606@users.noreply.github.com>
2026-05-27 18:16:59 +0800

    feat: add 5C-FILM+5C-FILM, 7C-FILM+5C-FILM, 7C-FILM+7C-FILM

--- js/config.js
+++ js/config.js
@@ -382,6 +382,9 @@ const FILM_FEES = {
   '8C-NOVACEL-LASER-FILM':7.7,
   '10C-NOVACEL-LASER-FILM':8.8,
   // 胶膜组合
+  '5C-FILM+5C-FILM':  2.0,
+  '7C-FILM+5C-FILM':  2.4,
+  '7C-FILM+7C-FILM':  2.8,
   '7C-LASER-FILM+5C-FILM':  3.0,
   '7C-LASER-FILM+7C-FILM': 3.4,
   '7C-ACHEM-LASER-FILM+5C-FILM':  5.0,
@@ -594,9 +597,9 @@ const FILM_ALIASES = {
   '纸': '垫纸',
   'paper': '垫纸',
   // 胶膜组合短名
-  '5c+5c': '5C+5C-FILM', '5c+5c-film': '5C+5C-FILM', '5c-film+5c-film': '5C+5C-FILM',
-  '7c+5c': '7C+5C-FILM', '7c+5c-film': '7C+5C-FILM', '7c-film+5c-film': '7C+5C-FILM',
-  '7c+7c': '7C+7C-FILM', '7c+7c-film': '7C+7C-FILM', '7c-film+7c-film': '7C+7C-FILM',
+  '5c+5c': '5C-FILM+5C-FILM', '5c+5c-film': '5C-FILM+5C-FILM', '5c-film+5c-film': '5C-FILM+5C-FILM',
+  '7c+5c': '7C-FILM+5C-FILM', '7c+5c-film': '7C-FILM+5C-FILM', '7c-film+5c-film': '7C-FILM+5C-FILM',
+  '7c+7c': '7C-FILM+7C-FILM', '7c+7c-film': '7C-FILM+7C-FILM', '7c-film+7c-film': '7C-FILM+7C-FILM',
   '5c蓝色': 'BLUE-5C-FILM', '5c蓝': 'BLUE-5C-FILM', '5c蓝膜': 'BLUE-5C-FILM', '蓝膜': 'BLUE-5C-FILM', '5c blue': 'BLUE-5C-FILM', '5c-blue-film': 'BLUE-5C-FILM',
   '7c-laser+5c': '7C-LASER+5C-FILM', '7c-laser+5c-film': '7C-LASER+5C-FILM', '7c laser-film+5c-film': '7C-LASER+5C-FILM', '7c激光膜+5c': '7C-LASER+5C-FILM', '7c激光膜+5c膜': '7C-LASER+5C-FILM',
   '胶膜': '7C-FILM',
